361cd59aa833d274ea2c8a6e1385916941701956
Willian-Yudy-F <futemawillian.aud@gmail.com>
2026-03-29 22:12:40 +1100

    Update sitemap.php

--- advweb/sitemap.php
+++ advweb/sitemap.php
@@ -1,10 +1,10 @@
 <?php include 'navbar.php'; ?>
 <div style="font-family: Arial; padding: 20px;">
-    <h1>Mapa do Site</h1>
+    <h1>Site Map</h1>
     <ul>
-        <li><a href="index.php">Página Inicial</a></li>
-        <li><a href="login.php">Acesso ao Sistema (Login)</a></li>
-        <li><a href="register.php">Cadastro de Usuário</a></li>
-        <li><a href="sitemap.php">Mapa do Site</a></li>
+        <li><a href="index.php">Home page</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">User Registration</a></li>
+        <li><a href="sitemap.php">Site Map</a></li>
     </ul>
 </div>
